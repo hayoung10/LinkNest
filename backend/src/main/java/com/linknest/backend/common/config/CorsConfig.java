5ca8074f512eb428cc45704e24783ca8a2c60f59
hayoung10 <mhymsy@gmail.com>
2025-09-14 20:42:38 +0900

    chore(backend): config 패키지를 common 하위로 이동

--- backend/src/main/java/com/linknest/backend/common/config/CorsConfig.java
+++ backend/src/main/java/com/linknest/backend/common/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.linknest.backend.config;
+package com.linknest.backend.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
