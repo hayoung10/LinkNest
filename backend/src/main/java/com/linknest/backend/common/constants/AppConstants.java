91ca47744f0b7cbbf05f5ef532b12b171ce3eb31
hayoung10 <mhymsy@gmail.com>
2025-09-15 21:16:22 +0900

    feat(backend): 전역 상수 관리용 constants 패키지 추가

---
+++ backend/src/main/java/com/linknest/backend/common/constants/AppConstants.java
@@ -0,0 +1,16 @@
+package com.linknest.backend.common.constants;
+
+public final class AppConstants {
+    private AppConstants() {}
+
+    // Security
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+
+    // API
+    public static final String API_PREFIX = "/api";
+
+    // Pagination
+    public static final int DEFAULT_PAGE_SIZE = 10;
+    public static final int MAX_PAGE_SIZE = 100;
+}
