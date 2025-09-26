5803a93292038e3259fba5cfd12ac65393a221cf
hayoung10 <mhymsy@gmail.com>
2025-09-26 20:20:33 +0900

    feat(backend): CustomOAuth2UserService 구현

---
+++ backend/src/main/java/com/linknest/backend/security/oauth2/CustomOAuth2UserService.java
@@ -0,0 +1,84 @@
+package com.linknest.backend.security.oauth2;
+
+import com.linknest.backend.user.User;
+import com.linknest.backend.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        // 원본 사용자 정보 조회
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        // provider 식별
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String nameAttributeKey = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        Map<String, Object> attributes = new HashMap<>(oAuth2User.getAttributes());
+
+        // provider별 응답(attributes)을 UserInfo로 변환
+        OAuth2UserInfo info = toUserInfo(registrationId, attributes);
+
+        // TODO: 이메일 검증 기능 구현 후 주석 해제
+//        if(!info.emailVerified()) {
+//            throw new OAuth2AuthenticationException(
+//                    new OAuth2Error("email_not_verified"),
+//                    "Email is not verified"
+//            );
+//        }
+
+        // 기존 User가 존재하면 갱신, 없으면 새로 생성
+        User user = findOrCreateUser(info);
+        Collection<GrantedAuthority> authorities =
+                List.of(new SimpleGrantedAuthority(user.getRole().name()));
+
+        // 핸들러에서 사용하는 부가 속성
+        attributes.put("provider", info.getProvider().name());
+        attributes.put("providerId", info.getProviderId());
+        attributes.put("userId", user.getId());
+
+        return new DefaultOAuth2User(authorities, attributes, nameAttributeKey);
+    }
+
+    private OAuth2UserInfo toUserInfo(String registrationId, Map<String, Object> attributes) {
+        return switch (registrationId) {
+            case "google" -> GoogleUserInfo.from(attributes);
+            default -> throw new OAuth2AuthenticationException(
+                    new OAuth2Error("unsupported_provider"),
+                    "Unsupported provider: " + registrationId
+            );
+        };
+    }
+
+    private User findOrCreateUser(OAuth2UserInfo info) {
+        Optional<User> optionalUser = userRepository.findByProviderAndProviderId(
+                info.getProvider(), info.getProviderId());
+        if(optionalUser.isPresent()) {
+            return optionalUser.get();
+        }
+
+        User user = User.builder()
+                .email(info.getEmail())
+                .name(info.getName())
+                .profileImageUrl(info.getPicture())
+                .provider(info.getProvider())
+                .providerId(info.getProviderId())
+                .build();
+        return user;
+    }
+}
