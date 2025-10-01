9a8f3b0cc1aad04cd4a4602ef34ac804290729f9
hayoung10 <mhymsy@gmail.com>
2025-10-01 21:14:10 +0900

    chore(backend): Redis 설정 추가 및 timeout 추가 - RedisConfig 생성 - application.yml에 spring.data.redis.timeout 속성 추가 - dump.rdb를 .gitignore에 추가

---
+++ backend/src/main/java/com/linknest/backend/config/RedisConfig.java
@@ -0,0 +1,61 @@
+package com.linknest.backend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.linknest.backend.auth.token.RefreshTokenEntity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        return new LettuceConnectionFactory();
+//    }
+
+    // Refresh Token 전용
+    @Bean(name = "refreshTokenRedisTemplate")
+    public RedisTemplate<String, RefreshTokenEntity> refreshTokenRedisTemplate(RedisConnectionFactory connectionFactory,
+                                                                   ObjectMapper objectMapper) {
+        RedisTemplate<String, RefreshTokenEntity> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Key Serializer
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        // Value Serializer
+        Jackson2JsonRedisSerializer<RefreshTokenEntity> serializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, RefreshTokenEntity.class);
+        template.setValueSerializer(serializer);
+        template.setHashValueSerializer(serializer);
+
+        return template;
+    }
+
+    // 범용
+    @Bean(name = "genericRedisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory, ObjectMapper objectMapper) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+
+        return template;
+    }
+
+    // 간단한 문자열
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+        return new StringRedisTemplate(connectionFactory);
+    }
+}
