82ce697a90c283abd5bfcce3bacd0e55fe92a50e
hayoung10 <mhymsy@gmail.com>
2025-11-22 12:19:16 +0900

    fix(backend): 북마크 수정 시 null 값으로 필드 초기화 허용 - title/description 필드를 null로 보내면 기존 값이 삭제되도록 수정

--- backend/src/main/java/com/linknest/backend/bookmark/BookmarkMapper.java
+++ backend/src/main/java/com/linknest/backend/bookmark/BookmarkMapper.java
@@ -11,7 +11,7 @@ public interface BookmarkMapper {
     Bookmark toEntity(BookmarkCreateReq createReq);
 
     // UpdateReq -> Entity (null 값은 무시)
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
     void updateFromDto(BookmarkUpdateReq updateReq, @MappingTarget Bookmark target);
 
     // Entity -> Res
